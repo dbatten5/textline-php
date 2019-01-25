ef37bb956128c2c3b58af80339e4fe77abead498
Dominic Batten <db@mettrr.com>
2019-01-25 14:11:33 +0000

    add phpunit dependecy and response class

---
+++ src/Http/Response.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Textline\Http;
+
+class Response
+{
+    /**
+     * @var mixed
+     */
+    protected $statusCode;
+
+    /**
+     * @var mixed
+     */
+    protected $content;
+
+    public function __construct($statusCode, $content)
+    {
+        $this->statusCode = $statusCode;
+        $this->content = $content;
+    }
+
+    /**
+     * Getter for statusCode
+     *
+     * @return string
+     * @author Dom Batten <db@mettrr.com>
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * Getter for content
+     *
+     * @return string
+     * @author Dom Batten <db@mettrr.com>
+     */
+    public function getContent()
+    {
+        return json_decode($this->content, true);
+    }
+
+    /**
+     * Return whether the response was successful
+     *
+     * @return bool
+     * @author Dom Batten <db@mettrr.com>
+     */
+    public function successful()
+    {
+        return $this->statusCode < 400;
+    }
+}
---
+++ tests/Http/ResponseTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use Textline\Http\Response;
+
+class ResponseTest extends TestCase
+{
+    /** @test */
+    public function a_response_has_a_status_code_and_content()
+    {
+        $code = 200;
+        $contentData = [
+            'content' => 'here'
+        ];
+        $content = json_encode($contentData);
+
+        $response = new Response($code, $content);
+
+        $this->assertEquals([
+            $code, $contentData
+        ], [
+            $response->getStatusCode(),
+            $response->getContent(),
+        ]);
+    }
+
+    /** @test */
+    public function it_can_confirm_whether_the_response_was_successful()
+    {
+        $code = 200;
+        $contentData = [
+            'content' => 'here'
+        ];
+        $content = json_encode($contentData);
+
+        $response = new Response($code, $content);
+
+        $this->assertTrue($response->successful());
+    }
+
+    /** @test */
+    public function it_can_confirm_whether_the_response_was_unsuccessful()
+    {
+        $code = 400;
+        $contentData = [
+            'content' => 'here'
+        ];
+        $content = json_encode($contentData);
+
+        $response = new Response($code, $content);
+
+        $this->assertFalse($response->successful());
+    }
+}
